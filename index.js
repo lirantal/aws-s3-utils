9407c6de96746423796169352cb78844e39d41b9
Liran Tal <liran.tal@gmail.com>
2017-05-30 00:25:18 +0300

    feat(downloadtostring): downloadToString library and initial tests coverage

--- index.js
+++ index.js
@@ -1,3 +1,88 @@
 'use strict'
 
-module.exports = (a, b) =>  a + b
+const fs = require('fs')
+const path = require('path')
+const AWS = require('aws-sdk')
+
+class awsS3Util {
+  /**
+   * validate configuration options passed to class methods
+   * @param   {Map} options - configuration options
+   * @returns {Boolean|Error}
+   */
+  static validateBasicOptions (options) {
+    if (options instanceof Map === false) {
+      return new Error('parameter must be a Map with config and object keys')
+    }
+
+    const config = options.get('config')
+
+    if (!config || !config.credentials) {
+      return new Error('config is required with a credentials object')
+    }
+
+    if (!config.credentials.secretAccessKey || !config.credentials.accessKeyId) {
+      return new Error('missing aws credentials for secret or access key')
+    }
+
+    const object = options.get('object')
+    if (!object.Bucket || !object.Key) {
+      return new Error('missing S3 object parameters with bucket and key')
+    }
+
+    return true
+  }
+
+  /**
+   * create an instance of AWS service
+   * @param   {Map} config - configuration to instantiate the service
+   * @returns {AWS.S3} an instance of AWS.S3 from aws-sdk
+   */
+  static initAWS (config = new Map()) {
+    AWS.config.update(config.get('config'))
+    return new AWS.S3()
+  }
+
+  /**
+   * downloads the content of an s3 object into a string
+   * @param   {Map} options - configuration parameters for AWS service
+   * @returns {Promise}
+   */
+  static downloadToString (options = new Map()) {
+    let error = this.validateBasicOptions(options)
+    if (error !== true) {
+      return Promise.reject(error)
+    }
+
+    return new Promise((resolve, reject) => {
+      const s3 = this.initAWS(options)
+      const s3Params = options.get('object')
+
+      let s3Metadata = ''
+      let fileStream = {}
+
+      fileStream = s3.getObject(s3Params).createReadStream()
+
+      const maxSize = options.get('maxSize')
+      const maxSizeEncoding = options.get('maxSizeEncoding')
+
+      fileStream.on('data', data => {
+        s3Metadata += data.toString()
+
+        if (maxSize && Buffer.byteLength(s3Metadata, maxSizeEncoding || 'utf-8') >= maxSize) {
+          return reject(new Error('string size exceeded'))
+        }
+      })
+
+      fileStream.on('end', () => {
+        return resolve(s3Metadata)
+      })
+
+      fileStream.on('error', (error) => {
+        return reject(error)
+      })
+    })
+  }
+}
+
+module.exports = awsS3Util
--- test/test.js
+++ test/test.js
@@ -1,7 +1,308 @@
 'use strict'
 
-const sum = require('../index')
+const fs = require('fs')
+const stream = require('stream')
+const sinon = require('sinon')
+const AWS = require('aws-sdk')
+const awsS3Util = require('../index.js')
 
-test('adds 1 + 2 = 3', () => {
-  expect(sum(1, 2)).toBe(3)
+const test = global.test
+const expect = global.expect
+
+describe('Download To String', () => {
+  it('should download the contents of an s3 object to a string', (done) => {
+    const mockStr1 = 'hello 1'
+    const mockStr2 = 'hello 2'
+    const myStub = sinon.stub()
+    myStub.returns({
+      createReadStream: () => {
+        // throw new Error('made up error')
+
+        const mockedStream = new stream.Readable()
+        mockedStream._read = () => {}
+
+        setTimeout(() => {
+          mockedStream.emit('data', mockStr1)
+          mockedStream.emit('data', mockStr2)
+          // mockedStream.emit('error', 'Hello data!')
+
+          setTimeout(() => {
+            mockedStream.emit('end', {})
+          }, 200)
+        }, 100)
+
+        return mockedStream
+      }
+    })
+    let getObjectOrig = AWS.S3.prototype.getObject
+    AWS.S3.prototype.getObject = myStub
+
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+    // params.set('config', { credentials: { accessKeyId: config.accessKeyId, secretAccessKey: config.secretAccessKey }} )
+
+    params.set('object', { Bucket: 'somebucket', Key: 'filekey' })
+    // params.set('object', { Bucket: config.s3.bucket, Key: config.s3.fileKey })
+
+    // params.set('maxSize', 1000)
+
+    awsS3Util.downloadToString(params)
+      .then((result) => {
+        expect(result).toBe(mockStr1 + mockStr2)
+
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        // restore stub to original object
+        AWS.S3.prototype.getObject = getObjectOrig
+        validateOptionsSpy.restore()
+
+        done()
+      })
+  })
+
+  it('should throw an error if stream throws an error', (done) => {
+    const mockErrorMsg = 'made up test error'
+    const myStub = sinon.stub()
+    myStub.returns({
+      createReadStream: () => {
+        throw new Error(mockErrorMsg)
+      }
+    })
+    let getObjectOrig = AWS.S3.prototype.getObject
+    AWS.S3.prototype.getObject = myStub
+
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+    params.set('object', { Bucket: 'somebucket', Key: 'filekey' })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        // restore stub to original object
+        AWS.S3.prototype.getObject = getObjectOrig
+        validateOptionsSpy.restore()
+
+        expect(error.message).toBe(mockErrorMsg)
+        done()
+      })
+  })
+
+  it('should throw an error if stream emits an error', (done) => {
+    const mockStr1 = 'hello 1'
+    const mockStr2 = 'hello 2'
+    const mockErrorStr = 'hello error str'
+    const myStub = sinon.stub()
+    myStub.returns({
+      createReadStream: () => {
+        const mockedStream = new stream.Readable()
+        mockedStream._read = () => {}
+
+        setTimeout(() => {
+          mockedStream.emit('data', mockStr1)
+          mockedStream.emit('data', mockStr2)
+
+          setTimeout(() => {
+            mockedStream.emit('error', mockErrorStr)
+          }, 200)
+        }, 100)
+
+        return mockedStream
+      }
+    })
+    let getObjectOrig = AWS.S3.prototype.getObject
+    AWS.S3.prototype.getObject = myStub
+
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+    params.set('object', { Bucket: 'somebucket', Key: 'filekey' })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        // restore stub to original object
+        AWS.S3.prototype.getObject = getObjectOrig
+        validateOptionsSpy.restore()
+
+        expect(error).toBe(mockErrorStr)
+        done()
+      })
+  })
+
+  it('should throw an error if params object is missing config key', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('something', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('config is required with a credentials object')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is missing accessKeyId credentials', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { secretAccessKey: 'key' } })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('missing aws credentials for secret or access key')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is missing secretAccessKey credentials', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'access' } })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('missing aws credentials for secret or access key')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is missing actual object keys', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+    params.set('object', {})
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('missing S3 object parameters with bucket and key')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is missing object bucket key', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+    params.set('object', { Key: 'filekey' })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('missing S3 object parameters with bucket and key')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is missing object Key key', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    const params = new Map()
+    params.set('config', { credentials: { accessKeyId: 'fake', secretAccessKey: 'key' } })
+    params.set('object', { Bucket: 'bucket' })
+
+    awsS3Util.downloadToString(params)
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('missing S3 object parameters with bucket and key')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is completely missing', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    awsS3Util.downloadToString()
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('config is required with a credentials object')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is an empty object', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    awsS3Util.downloadToString({})
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('parameter must be a Map with config and object keys')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
+
+  it('should throw an error if params object is an empty object', (done) => {
+    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+
+    awsS3Util.downloadToString({})
+      .then()
+      .catch((error) => {
+        // assert validateOptions method was called
+        sinon.assert.called(validateOptionsSpy)
+
+        expect(error.message).toBe('parameter must be a Map with config and object keys')
+
+        validateOptionsSpy.restore()
+        done()
+      })
+  })
 })
