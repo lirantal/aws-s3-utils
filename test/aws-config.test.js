47afdc2e40e0f17548da8685ccad26f2c7a44c93
Liran Tal <liran.tal@gmail.com>
2017-06-02 01:42:35 +0300

    refactor(jest): using jest to mock tests, removing sinon (#1)

--- test/aws-config.test.js
+++ test/aws-config.test.js
@@ -1,11 +1,10 @@
 'use strict'
 
-const sinon = require('sinon')
 const awsS3Util = require('../index.js')
 
 describe('Download To String - AWS Exceptions', () => {
   it('should throw an error if AWS wasnt able to instantiate successfully', (done) => {
-    const validateOptionsSpy = sinon.spy(awsS3Util, 'validateBasicOptions')
+    const spy = jest.spyOn(awsS3Util, 'validateBasicOptions')
 
     const params = new Map()
     params.set('config', { credentials: { accessKeyId: {obj: 'a'}, secretAccessKey: 'key' } })
@@ -14,13 +13,11 @@ describe('Download To String - AWS Exceptions', () => {
     awsS3Util.downloadToString(params)
       .then()
       .catch((error) => {
-        // assert validateOptions method was called
-        sinon.assert.called(validateOptionsSpy)
-
-        // console.log(error)
         expect(error.message).toBe(`Authorization header is invalid -- one and only one ' ' (space) required`)
+        expect(spy).toHaveBeenCalled()
 
-        validateOptionsSpy.restore()
+        spy.mockReset()
+        spy.mockRestore()
         done()
       })
   })
